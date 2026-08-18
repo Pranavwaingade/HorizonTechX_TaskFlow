aaed5dbedcab41c0df6b89dfa1dd8b333d72c98c
Pranav Waingade <pranavwaingade3@gmail.com>
2026-08-18 21:11:31 +0530

    fix netlify react router routes

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server Running on http://localhost:${PORT}`);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL:
-        import.meta.env.VITE_API_URL ||
-        "",
+        import.meta.env.VITE_API_URL
 });
 
 API.interceptors.request.use((config) => {
